758df61ed8763d21bcbacdd85636a6fadd024a73
Niklas Lausch <niki@lauschig.net>
2024-03-31 17:14:27 +0200

    feat: added analytics

--- app.vue
+++ app.vue
@@ -13,4 +13,17 @@ useSeoMeta({
   title: 'Papaplatte Games',
   description: 'Hier findest du alle Spiele die Papaplatte noch spielen will.',
 });
+
+useHead({
+  script: [
+    {
+      src: 'https://analytics.niki2k1.dev/js/script.js',
+      defer: true,
+      'data-domain': 'games.kommtkevinonline.de',
+    },
+  ],
+  htmlAttrs: {
+    lang: 'de',
+  },
+});
 </script>
