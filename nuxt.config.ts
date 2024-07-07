7c4db10c864f881214e464110edaf9d9f59c85d6
Niklas Lausch <niki@lauschig.net>
2024-07-07 20:42:40 +0200

    chore: moved some folders to app/ dir chore: activated v4 compatibility version

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,18 +1,27 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
+
+  future: {
+    compatibilityVersion: 4,
+  },
+  compatibilityDate: '2024-07-07',
+
   extends: ['@nuxt/ui-pro'],
+
   modules: [
     '@nuxt/ui',
     '@nuxt/content',
     '@nuxtjs/color-mode',
     '@nuxthq/studio',
-    "@nuxthub/core",
-    "nuxt-auth-utils"
+    '@nuxthub/core',
+    'nuxt-auth-utils',
   ],
+
   colorMode: {
     preference: 'dark',
   },
+
   ui: {
     icons: ['mdi', 'simple-icons'],
   },
